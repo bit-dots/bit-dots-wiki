8910a49b73c3bec722bca249b97edb8697d732e2
bit-dots <288258344+bit-dots@users.noreply.github.com>
2026-06-23 22:46:44 +0800

    🐞 fix(css): 修复链接背景色样式，移除不必要的 !important

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,33 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
+import sitemap from '@astrojs/sitemap';
+import { remarkContainers } from './src/plugins/remark-containers.mjs';
+import { rehypeBadges as remarkBadges } from './src/plugins/rehype-badges.mjs';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue()],
+  site: 'https://bit-dots.com',
+  integrations: [
+    vue(),
+    sitemap({
+      filter: (page) => !page.includes('/404')
+    }),
+  ],
+  markdown: {
+    remarkPlugins: [
+      remarkContainers,
+      remarkBadges,
+    ],
+    rehypePlugins: [
+    ],
+    syntaxHighlight: 'shiki',
+    shikiConfig: {
+      themes: {
+        light: 'github-light',
+        dark: 'github-dark',
+      },
+      langs: [],
+      wrap: true,
+    },
+  },
 });
